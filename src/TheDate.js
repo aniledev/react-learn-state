78800ef366c2461193e8cb76e5c207e113bbf5ec
aniledev <elinamcgill@gmail.com>
2020-12-24 19:34:41 -0500

    feat: render TheData component accurately to the DOM

--- src/TheDate.js
+++ src/TheDate.js
@@ -1,13 +1,18 @@
 import React from "react";
 
+// think of classes like a blueprint
 class TheDate extends React.Component {
   constructor(props) {
     super(props);
-    // state is usually set as an object; think of state as the STORE data in earlier projects
-    this.state = {};
+    // initial state is usually set as an object; think of state as the STORE data in earlier projects
+    this.state = {
+      datetime: new Date(),
+    };
   }
   render() {
-    return <div></div>;
+    console.log(this.state);
+    // toLocaleString() displays the date in a human-readable format
+    return <div>{this.state.datetime.toLocaleString()}</div>;
   }
 }
 
