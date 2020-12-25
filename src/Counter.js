8ab70cd4d85269c3902eea99f19ef3276b5ed368
aniledev <elinamcgill@gmail.com>
2020-12-24 20:40:23 -0500

    feat: add event listener as a prop to button

--- src/Counter.js
+++ src/Counter.js
@@ -4,11 +4,16 @@ class Counter extends React.Component {
   state = {
     count: 0,
   };
+
+  handleButtonClick() {
+    console.log("clicked!");
+  }
+
   render() {
     return (
       <div>
         <p>The current count: {this.state.count}</p>
-        <button>Add 1</button>
+        <button onClick={this.handleButtonClick}>Add 1</button>
       </div>
     );
   }
