1e01b86b5551ee805a3cdc2380c5c2ab6f310fb3
aniledev <elinamcgill@gmail.com>
2020-12-24 20:33:30 -0500

    chore: create Counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import TheDate from "./TheDate";
+import Counter from "./Counter";
 
 function App() {
   return (
     <div className="App">
       <TheDate />
+      <Counter />
     </div>
   );
 }
